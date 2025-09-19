65864625cba0c48841929926e2fe84d593d9cdbc
PhanAnhChu <aiengineercpa2004@gmail.com>
2025-09-19 09:24:22 +0700

    Initial support for DNSE. Add foreign buy/sell data. Fix wrong end candle error.

--- logic_processor.py
+++ logic_processor.py
@@ -1,24 +1,30 @@
-from global_var import ENTRADE_CLIENT #, LAST_BID_DEPTH, LAST_OFFER_DEPTH
-from datetime import datetime
+from agent import Agent
+import GLOBAL
 from agents.ma_cross_agent import ma_cross_agent
 from agents.BB_agent import bb_agent
-from agent import Agent
+from datetime import datetime
 
 ACTIVE_BOT: list[Agent] = [
     ma_cross_agent,
     bb_agent
 ]
 
-def CalculateStrategy(processed_data):
-    close_price = processed_data[-1][3]
-    # print(LAST_BID_DEPTH) // Market depth data
-    # print(LAST_OFFER_DEPTH)
+def OnTick(data: tuple):
+    print("Dư mua:", GLOBAL.TOTAL_BID)
+    print("Dư bán:", GLOBAL.TOTAL_OFFER)
+    print("Tổng KLGD mua nước ngoài:", GLOBAL.TOTAL_FOREIGN_BUY)
+    print("Tổng KLGD bán nước ngoài:", GLOBAL.TOTAL_FOREIGN_SELL)
+
+def OnBarClosed(data: list[tuple]):
+    close_price = data[-1][3]
+    # print("Độ sâu mua:", GLOBAL.BID_DEPTH[0]) // Market depth data
+    # print("Độ sâu bán:", GLOBAL.OFFER_DEPTH[0])
 
     for agent in ACTIVE_BOT:
-        result = agent.Calculate(processed_data)
+        result = agent.Calculate(data)
         if result == True: # MUST CHECK FOR BOOL, SINCE AGENT MAY RETURN NONE TOO!
-            ENTRADE_CLIENT.Order("VN30F2509", "NB", None, None, 1, "MTL", True)
+            GLOBAL.ENTRADE_CLIENT.Order("VN30F2509", "NB", None, None, 1, "MTL", True)
             print(f"{agent.name} đã đặt lệnh LONG tại giá {close_price:.1f} ({datetime.now().strftime("%H:%M %d/%m")})")
         elif result == False:
-            ENTRADE_CLIENT.Order("VN30F2509", "NS", None, None, 1, "MTL", True)
+            GLOBAL.ENTRADE_CLIENT.Order("VN30F2509", "NS", None, None, 1, "MTL", True)
             print(f"{agent.name} đã đặt lệnh SHORT tại giá {close_price:.1f} ({datetime.now().strftime("%H:%M %d/%m")})")
--- technical_analysis.py
+++ technical_analysis.py
@@ -1,22 +1,20 @@
 import numpy as np
 
-def MA(data: np.ndarray, feature_idx: int = 3) -> np.ndarray:
+def MA(data: np.ndarray, feature_idx: int = 3) -> float | None:
     '''
     data: (m, n) np.ndarray
     '''
     _, n = data.shape
-
     if -n <= feature_idx < n:
         return np.mean(data[:, feature_idx]).item()
 
     return None
 
-def STD(data: np.ndarray, feature_idx: int = 3) -> np.ndarray:
+def STD(data: np.ndarray, feature_idx: int = 3) -> float | None:
     '''
     data: (m, n) np.ndarray
     '''
     _, n = data.shape
-
     if -n <= feature_idx < n:
         return np.std(data[:, feature_idx]).item()
 
