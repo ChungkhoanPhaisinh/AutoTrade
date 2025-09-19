0b702cba894fb5417e5eaf0ee0f623b713f29a57
PhanAnhChu <aiengineercpa2004@gmail.com>
2025-09-19 09:33:47 +0700

    Change to correct symbol

--- logic_processor.py
+++ logic_processor.py
@@ -10,10 +10,11 @@ ACTIVE_BOT: list[Agent] = [
 ]
 
 def OnTick(data: tuple):
-    print("Dư mua:", GLOBAL.TOTAL_BID)
-    print("Dư bán:", GLOBAL.TOTAL_OFFER)
-    print("Tổng KLGD mua nước ngoài:", GLOBAL.TOTAL_FOREIGN_BUY)
-    print("Tổng KLGD bán nước ngoài:", GLOBAL.TOTAL_FOREIGN_SELL)
+    # print("Dư mua:", GLOBAL.TOTAL_BID)
+    # print("Dư bán:", GLOBAL.TOTAL_OFFER)
+    # print("Tổng KLGD mua nước ngoài:", GLOBAL.TOTAL_FOREIGN_BUY)
+    # print("Tổng KLGD bán nước ngoài:", GLOBAL.TOTAL_FOREIGN_SELL)
+    pass
 
 def OnBarClosed(data: list[tuple]):
     close_price = data[-1][3]
@@ -23,8 +24,8 @@ def OnBarClosed(data: list[tuple]):
     for agent in ACTIVE_BOT:
         result = agent.Calculate(data)
         if result == True: # MUST CHECK FOR BOOL, SINCE AGENT MAY RETURN NONE TOO!
-            GLOBAL.ENTRADE_CLIENT.Order("VN30F2509", "NB", None, None, 1, "MTL", True)
+            GLOBAL.ENTRADE_CLIENT.Order("41I1FA000", "NB", None, None, 1, "MTL", True)
             print(f"{agent.name} đã đặt lệnh LONG tại giá {close_price:.1f} ({datetime.now().strftime("%H:%M %d/%m")})")
         elif result == False:
-            GLOBAL.ENTRADE_CLIENT.Order("VN30F2509", "NS", None, None, 1, "MTL", True)
+            GLOBAL.ENTRADE_CLIENT.Order("41I1FA000", "NS", None, None, 1, "MTL", True)
             print(f"{agent.name} đã đặt lệnh SHORT tại giá {close_price:.1f} ({datetime.now().strftime("%H:%M %d/%m")})")
